20d12a844c1f66c01a9416f36be5a47005ab8aad
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-29 19:48:16 +0100

    better formatting in printing out the error message

--- runner.js
+++ runner.js
@@ -21,7 +21,7 @@ class Runner {
           console.log(`OK - ${desc}`)
         } catch (err) {
           console.log(`X - ${desc}`)
-          console.log(err);
+          console.log('\t', err.message)
         }
       }
 
