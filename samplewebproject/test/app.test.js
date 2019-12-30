310fe2b3aaf4a336d496e71ab7c112841fd824fd
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-30 18:21:50 +0100

    started creating a test to check if the validation works fine

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -6,3 +6,11 @@ it('has a text input', async () => {
 
   assert(input)
 })
+
+it('show a success message with a valid email', async () => {
+  const dom = await render('index.html')
+
+  const input = dom.window.document.querySelector('input')
+  input.value = 'fsdfds@fssadf.com'
+
+})
