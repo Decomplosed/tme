df46902695eed8898c0aa4e2ea6b1e8c59fd5559
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-30 17:47:56 +0100

    triggered the h1 under header variable

--- samplewebproject/index.js
+++ samplewebproject/index.js
@@ -2,10 +2,11 @@ document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault()
 
   const { value } = document.querySelector('input')
+  const header = document.querySelector('h1')
 
-  if(value.includes('@')) {
+  if (value.includes('@')) {
 
   } else {
-    
+
   }
 })
