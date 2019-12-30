efaf97f704224486601ec422304c958e693440e5
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-30 17:47:00 +0100

    started creating the validation to chedk if the input value contains @ character

--- samplewebproject/index.js
+++ samplewebproject/index.js
@@ -2,4 +2,10 @@ document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault()
 
   const { value } = document.querySelector('input')
+
+  if(value.includes('@')) {
+
+  } else {
+    
+  }
 })
