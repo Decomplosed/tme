c1ea768763a3016571284ca521d0613b175d93c8
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-30 18:02:15 +0100

    connected render with runner.js file

--- runner.js
+++ runner.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
+const render = require('./render')
 
 const forbiddenDirs = ['node_modules']
 
@@ -14,7 +15,7 @@ class Runner {
       console.log(chalk.gray(`----${file.shortName}`))
 
       const beforeEaches = []
-
+      global.render = render
       global.beforeEach = (fn) => {
         beforeEaches.push(fn)
       }
