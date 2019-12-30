d9d12d060e9893776dc36bfde19349e18cc4ad02
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-30 18:11:14 +0100

    HTML elements assertion

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,5 +1,8 @@
+const assert = require('assert')
+
 it('has a text input', async () => {
   const dom = await render('index.html')
+  const input = dom.window.document.querySelector('input')
 
-  console.log(dom)
+  assert(input)
 })
